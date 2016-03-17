bbc3c7e6c7c21662147535c802aaa01a57e75068
Vinay Selvaraj <vinas@amazon.com>
2016-03-17 09:47:14 -0400

    Redirect diff output to /dev/null

--- generate-haproxy-cfg.sh
+++ generate-haproxy-cfg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/local/bin/docker_proxy.py > /tmp/haproxy.cfg-docker
-diff /tmp/haproxy.cfg-docker /etc/haproxy/haproxy.cfg
+diff /tmp/haproxy.cfg-docker /etc/haproxy/haproxy.cfg >& /dev/null
 
 if [ $? -ne 0 ]
 then
